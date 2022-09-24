c910b76be10adb1df772f4fd93ec80183ffb61ad
KodingNYoung <abiodunadebambo44@gmail.com>
2022-09-24 09:01:27 +0100

    finish setup and folder structure

---
+++ src/views/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// app components
+import Navbar from "../components/Navbar"
+import Hero from "./sections/hero"
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default Home
